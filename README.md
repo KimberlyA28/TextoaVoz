adbb479841044af818f6c90d16f1b035348a7376
Kimberly <lpareja972@gmail.com>
2024-07-06 22:48:44 -0500

    texto a voz

---
+++ README.md
@@ -0,0 +1,5 @@
+# Texto a Voz
+
+<a href="https://www.instagram.com/kimberly.eliene/">Instagram</a>.
+
+[![texto-voz.jpg](https://i.postimg.cc/PqJRTRPM/texto-voz.jpg)](https://postimg.cc/mcxwSV3c)
---
+++ script.js
@@ -0,0 +1,70 @@
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector("select"),
+speechBtn = document.querySelector("button");
+
+let synth = speechSynthesis,
+isSpeaking = true;
+
+voices();
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
+    }
+}
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text){
+    let utterance = new SpeechSynthesisUtterance(text);
+    for(let voice of synth.getVoices()){
+        if(voice.name === voiceList.value){
+            utterance.voice = voice;
+        }
+    }
+    synth.speak(utterance);
+}
+
+speechBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    if(textarea.value !== ""){
+        if(!synth.speaking){
+            textToSpeech(textarea.value);
+        }
+        if(textarea.value.length > 80){
+            setInterval(()=>{
+                if(!synth.speaking && !isSpeaking){
+                    isSpeaking = true;
+                    speechBtn.innerText = "Convertir a voz";
+                }else{
+                }
+            }, 500);
+            if(isSpeaking){
+                synth.resume();
+                isSpeaking = false;
+                speechBtn.innerText = "Pausar el discurso";
+            }else{
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerText = "Reanudar el discurso";
+            }
+        }else{
+            speechBtn.innerText = "Convertir a voz";
+        }
+    }
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
